238ed5d8048de2ff481ccbf380321511b298bdfa
IgorDoring <idoring@tutanota.com>
2023-07-28 05:33:33 -0300

    Iniciando componente pra teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <app-banking></app-banking>
+  `
 })
 export class AppComponent {
-  title = 'teste-unitario';
 
-  public soma(v1: number, v2:number){
-    return v1 + v2;
-  }
 }
