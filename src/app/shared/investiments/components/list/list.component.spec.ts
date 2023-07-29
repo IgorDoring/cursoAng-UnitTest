9e165ffa972c347dd1e143c86bdee5ed4303bd49
IgorDoring <idoring@tutanota.com>
2023-07-29 14:20:06 -0300

    Terminando testes

--- src/app/shared/investiments/components/list/list.component.spec.ts
+++ src/app/shared/investiments/components/list/list.component.spec.ts
@@ -2,10 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListComponent } from './list.component';
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {Investiments} from "../../model/investiments";
+import {MOCK_LIST} from "../../services/list-investiments.mock";
+import {ListInvestimentsService} from "../../services/list-investiments.service";
+import {of} from "rxjs";
 
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
+  let service: ListInvestimentsService;
+  const mockList: Array<Investiments> = MOCK_LIST;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,6 +22,7 @@ describe('ListComponent', () => {
     .compileComponents();
 
     fixture = TestBed.createComponent(ListComponent);
+    service = TestBed.inject(ListInvestimentsService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -25,17 +32,31 @@ describe('ListComponent', () => {
   });
 
   it('(U) - deve listar investimentos', function () {
+    spyOn(service, "list").and.returnValue(of(MOCK_LIST));
+    component.ngOnInit();
+    fixture.detectChanges();
     let investiments = component.investiments;
-    expect(investiments.length).toBe(4);
-    expect(investiments[0].name).toContain('Itaú');
-    expect(investiments[3].name).toContain('Nubank');
+
+    expect(service.list).toHaveBeenCalledWith();
+    expect(component.investiments.length).toEqual(5);
+
+    expect(investiments[0].name).toEqual("Banco 1");
+    expect(investiments[0].value).toBe(100);
+    expect(investiments[4].name).toEqual("Banco 5");
+    expect(investiments[4].value).toBe(100);
   });
 
 
   it('(I) - deve listar investimentos', function () {
+    spyOn(service, "list").and.returnValue(of(MOCK_LIST));
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
     let investiments = fixture.debugElement.nativeElement.querySelectorAll('.list-itens');
-    expect(investiments.length).toBe(4);
-    expect(investiments[0].textContent.trim()).toEqual('Itaú | 100');
-    expect(investiments[3].textContent.trim()).toEqual('Nubank | 100');
+
+    expect(investiments.length).toBe(5);
+    expect(investiments[0].textContent.trim()).toEqual('Banco 1 | 100');
+    expect(investiments[4].textContent.trim()).toEqual('Banco 5 | 100');
   });
 });
--- src/app/shared/investiments/services/list-investiments.service.spec.ts
+++ src/app/shared/investiments/services/list-investiments.service.spec.ts
@@ -4,19 +4,14 @@ import { ListInvestimentsService } from './list-investiments.service';
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import {HttpClient} from "@angular/common/http";
 import {Investiments} from "../model/investiments";
+import {MOCK_LIST} from "./list-investiments.mock";
 
 describe('ListInvestimentsService', () => {
   let service: ListInvestimentsService;
   let httpTestingController: HttpTestingController;
   let httpClient: HttpClient;
   const url: string = "https://raw.githubusercontent.com/troquatte/fake-server/main/investiments-all.json";
-  const mockList: Array<Investiments> = [
-    {name: "Banco 1", value: 100},
-    {name: "Banco 2", value: 100},
-    {name: "Banco 3", value: 100},
-    {name: "Banco 4", value: 100},
-    {name: "Banco 5", value: 100}
-  ];
+  const mockList: Array<Investiments> = MOCK_LIST;
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule]
@@ -32,5 +27,19 @@ describe('ListInvestimentsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('(U) - deve listar todos os investimentos', function (done) {
+    service.list().subscribe(
+      (res: Array<Investiments>) => {
+        expect(res[0].name).toEqual("Banco 1");
+        expect(res[0].value).toEqual(100);
+        done();
+      }
+    );
+    const req = httpTestingController.expectOne(url);
+    req.flush(mockList);
+
+    expect(req.request.method).toEqual("GET");
   });
 });
